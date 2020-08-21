94febcc21b732b3867334a462ecb25287f99369a
rdroosa <richardroosa2022@u.northwestern.edu>
2020-08-20 20:55:28 -0400

    WIP Canny Edge detector added non-maximal supression

--- code/si_canny.cu
+++ code/si_canny.cu
@@ -21,7 +21,9 @@ void k_canny (uchar *img, uchar *canny)
 	
 	register int not_tile_edge = !((threadIdx.x == 0) || (threadIdx.y == 0) || (threadIdx.x == 31) || (threadIdx.y == 29)); 
 	register int not_img_edge = ((row > 0) && (row < 2016) && (col > 0) && (col < 2940));
-	register int v, h; 
+	register int v, h;
+	register char dir, diag, is_max;
+	register float dir_weight, diag_weight; 
 	
 	__shared__ uchar intensity	[32][30];
 	__shared__ uchar sobel		[32][30];
@@ -58,14 +60,40 @@ void k_canny (uchar *img, uchar *canny)
 	
 	// Apply non-maximal suppression and threshold min/maxing over shared memory tile
 	
-	// __syncthreads();
+	if (not_tile_edge)
+	{
+		dir = abs(h) > abs(v);
+		diag = (( h > 0 ) == (v > 0));	
+		
+		dir_weight = dir * ((float) v) / ((float) h);
+		diag_weight = 1 - dir_weight;
+		
+		is_max = 
+				(sobel[threadIdx.x][threadIdx.y] > 
+					(dir_weight * sobel[threadIdx.x + dir][threadIdx.y + !dir]
+					+ diag_weight * sobel[threadIdx.x + diag - !diag][threadIdx.y + 1]))
+			&
+				(sobel[threadIdx.x][threadIdx.y] >
+					(dir_weight * sobel[threadIdx.x - dir][threadIdx.y - !dir]
+					+ diag_weight * sobel[threadIdx.x - diag + !diag][threadIdx.y - 1]));	
+					
+		nms[threadIdx.x][threadIdx.y] = 
+			is_max * 
+			(
+					(128 * (sobel[threadIdx.x][threadIdx.y] > 200))
+				+ 	
+					(127 * (sobel[threadIdx.x][threadIdx.y] > 200))
+			);
+	}
+	
+	__syncthreads();
 	
 	// Apply local hysteresis edge detection
 	
 	// Copy shared memory tile to global memory
 	if (not_img_edge & not_tile_edge)
 	{
-		canny[i_canny] = sobel[threadIdx.x][threadIdx.y];
+		canny[i_canny] = nms[threadIdx.x][threadIdx.y];
 	}
 }
 
@@ -95,6 +123,11 @@ int main(void)
     dim3 block(32, 30, 1);
     dim3 grid(98, 72, 1);
     
+    for (int i = 0; i < 100; i++)
+    {
+    	k_canny<<<grid, block>>>(img_in, canny);
+    }
+    
 	cudaEventRecord(start);
     k_canny<<<grid, block>>>(img_in, canny);
 	cudaEventRecord(stop);
@@ -112,10 +145,9 @@ int main(void)
     imshow("canny", img_out);
     waitKey(0);
 	
+	/*
 	clock_t startt, stopt;
-
 	Mat gray, cannyimg;
-
 	startt = clock();
 	cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 	cv::Canny(gray, cannyimg, 100, 200);
@@ -124,6 +156,7 @@ int main(void)
 	DEBUG_("CPU TIME: " << runtime)
 	imshow("canny", cannyimg);
 	waitKey(0);   
-
+	*/
+	
     return 0;
 }
