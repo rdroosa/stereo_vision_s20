e04c41acdb1ae3b06eb96b273394a4bf1a51fe36
rdroosa <richardroosa2022@u.northwestern.edu>
2020-08-21 23:48:34 -0400

    Added hysteresis threshold

--- code/si_canny.cu
+++ code/si_canny.cu
@@ -6,8 +6,15 @@
 
 #define DEBUG_(x) (std::cout << "DEBUG: " << x << std::endl);
 #define MOD (2016 * 2940 * 3)
+#define ROWS 2016
+#define COLS 2940
 
 using namespace cv;
+
+__device__ int rel_i (int index, int d_cols, int d_rows)
+{
+	return index + (d_rows * COLS) + d_cols;
+}
 
 __global__ void k_intensity (uchar *img_in, uchar *intensity)
 {
@@ -17,31 +24,106 @@ __global__ void k_intensity (uchar *img_in, uchar *intensity)
    intensity[o_index] = (img_in[i_index] + img_in[i_index + 1] + img_in[i_index + 2]) / 3; 
 }
 
-__global__ void k_sobel (uchar *intensity, uchar *sobel)
+__global__ void k_sobel (uchar *intensity, uchar *sobel, int high, int low)
 {
 	register int index = (960 * blockIdx.x) + threadIdx.x;
 	register int mod = index % 5927040;
-    
 
 	if ((index > 2939) && (index < 5924200) && (mod != 0) && (mod != 2939)) 
 	{
     	register int h, v, mag;
-		v = intensity[index - 2941] + 2 * intensity[index - 2940] +  intensity[index - 2939] 
-		- intensity[index + 29410] - 2 * intensity[index + 2940] - intensity[index + 2939];
+		register char dir, diag, is_max;
+		float dir_weight, diag_weight;
+		v = intensity[rel_i(index, -1, 1)] + 2 * intensity[rel_i(index, 0, 1)] +  intensity[rel_i(index, 1, 1)] 
+			- intensity[rel_i(index, -1, -1)] - 2 * intensity[rel_i(index, 0, -1)] - intensity[rel_i(index, 1, -1)];
 
-		h = intensity[index - 2941] + 2 * intensity[index - 1] +  intensity[index + 2939]
-			- intensity[index + 2941] - 2 * intensity[index + 1] - intensity[index - 2939];
-		
+		h = intensity[rel_i(index, 1, -1)] + 2 * intensity[rel_i(index, 1, 0)] +  intensity[rel_i(index, 1, 1)]
+			- intensity[rel_i(index, -1, -1)] - 2 * intensity[rel_i(index, -1, 0)] - intensity[rel_i(index, -1, 1)];
+
 		mag = v * v + h * h;
-	
-		sobel[index] = sqrt((float) mag);		
+
+		dir = abs(h) > abs(v);
+		diag = (( h > 0 ) == ( v > 0 ));	
+
+		dir_weight = dir * ((float) v) / ((float) h);
+		diag_weight = 1 - dir_weight;
+
+		mag = sqrt((float) mag);
+		sobel[index] = mag;
+
+		__syncthreads();
+
+		is_max = 
+				(mag > 
+					(dir_weight * sobel[rel_i(index, dir, !dir)]
+					+ diag_weight * sobel[rel_i(index, diag - !diag,  1)]))
+			&&
+				(mag >
+					(dir_weight * sobel[rel_i(index, -dir, -(!dir))]
+					+ diag_weight * sobel[rel_i(index, !diag - diag, -1)]));
+
+		__syncthreads();
+		
+		sobel[index] = is_max * 128 * (mag > low) + 127 * (mag > high);	
 	}
 }
 
-
-__global__ void k_hyst (uchar *sobel_h, uchar *hyster_thresh)
+__global__ void k_hyst_traverse (uchar *in, char *done)
 {
+	
+register int index = (960 * blockIdx.x) + threadIdx.x;
+	char is_done = 0;
+	if (in[index] == 255)
+	{
+		if (in[rel_i(index, -1, -1)] == 128) 
+		{
+			in[rel_i(index, -1, -1)] = 255;
+			is_done = 1;
+		}
+		if (in[rel_i(index, 0, -1)] == 128) 
+		{
+			in[rel_i(index, 0, -1)] = 255;
+			is_done = 1;
+		}
+		if (in[rel_i(index, 1, -1)] == 128) 
+		{
+			in[rel_i(index, 1, -1)] = 255;
+			is_done = 1;
+		}
+		if (in[rel_i(index, -1, 0)] == 128) 
+		{
+			in[rel_i(index, -1, 0)] = 255;
+			is_done = 1;
+		}
+		if (in[rel_i(index, 1, 0)] == 128) 
+		{
+			in[rel_i(index, 1, 0)] = 255;
+			is_done = 1;
+		}
+		if (in[rel_i(index, -1, 1)] == 128) 
+		{
+			in[rel_i(index, -1, 1)] = 255;
+			is_done = 1;
+		}
+		if (in[rel_i(index, 0, 1)] == 128) 
+		{
+			in[rel_i(index, 0, 1)] = 255;
+			is_done = 1;
+		}
+		if (in[rel_i(index, 1, 1)] == 128) 
+		{
+			in[rel_i(index, 1, 1)] = 255;
+			is_done = 1;
+		}
+	*done = is_done;
+	}
+	
+}
 
+__global__ void k_hyst_prune(uchar *in)
+{
+	register int index = (960 * blockIdx.x) + threadIdx.x;
+	in[index] = in[index] * (in[index] == 255);
 }
 
 int main(void)
@@ -51,68 +133,58 @@ int main(void)
     cudaEventCreate(&stop);
 
     Mat img = imread("test_data/Backpack-perfect/im0.png", IMREAD_COLOR);
-
-    namedWindow("high", WINDOW_NORMAL);
-	namedWindow("low", WINDOW_NORMAL);
-	namedWindow("intensity", WINDOW_NORMAL);
 	namedWindow("canny", WINDOW_NORMAL);
 
     int n_pix = img.rows * img.cols;
     int n_subpix = n_pix * img.channels();
+	char *done;
 
     uchar *img_in, *intensity, *sobel;
     
     cudaMallocManaged(&img_in, n_subpix * sizeof(img.data[0]));
     cudaMallocManaged(&intensity, n_pix * sizeof(img.data[0]));
 	cudaMallocManaged(&sobel, n_pix * sizeof(img.data[0]));
+	cudaMallocManaged(&done, sizeof(char)); 
 
     for (int i=0; i < n_subpix; i++)
     {
         img_in[i] = img.data[i];
     }
 
-	cudaEventRecord(start);
-    k_intensity<<<6174, 960>>>(img_in, intensity);
-	
-    //cudaDeviceSynchronize();
-	
-/*
-    Mat imgintensity = Mat(img.rows, img.cols, CV_8UC1, intensity);
-    DEBUG_("READ INT")
-    imshow("intensity", imgintensity);
-    waitKey(0); 
-*/
+	cudaDeviceSynchronize();
+	int n_runs = 100;
+	*done = 1;
+	float ms; 
+	float running = 0;
 
-    k_sobel<<<6174, 960>>>(intensity, sobel);
-    cudaDeviceSynchronize(); 
 
-	cudaEventRecord(stop);
-    cudaEventSynchronize(stop);
-    cudaDeviceSynchronize();
+	for (int i = 0; i < n_runs; i++)
+	{
+		cudaEventRecord(start);
+		k_intensity<<<6174, 960>>>(img_in, intensity);
+		k_sobel<<<6174, 960>>>(intensity, sobel, 75, 25);
+		cudaDeviceSynchronize();
+		*done = 1;
+		while (*done)
+		{
+			k_hyst_traverse<<<6174, 960>>>(sobel, done);
+			cudaDeviceSynchronize();	
+		}
+		k_hyst_prune<<<6174, 960>>>(sobel);
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		cudaDeviceSynchronize(); 
+		cudaEventElapsedTime(&ms, start, stop);
+
+		running += ms;
+	}
    
-    float ms;
+    DEBUG_("AVERAGE: " << running / n_runs << std::endl << "LAST: " << ms) 
 
-    cudaEventElapsedTime(&ms, start, stop);
-
-    DEBUG_("KERNEL TIME: " << ms) 
-	
-	Mat high = Mat(img.rows, img.cols, CV_8UC1, sobel);
+	img = Mat(img.rows, img.cols, CV_8UC1, sobel);
     DEBUG_("READ IMGS")
-    imshow("high", high);
+    imshow("canny", img);
     waitKey(0);
-	
-	clock_t startt, stopt;
-
-	Mat gray, cannyimg;
-
-	startt = clock();
-	cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
-	cv::Canny(gray, cannyimg, 100, 200);
-	stopt = clock();
-	double runtime = double(stopt - startt) / double(CLOCKS_PER_SEC);
-	DEBUG_("CPU TIME: " << runtime)
-	imshow("canny", cannyimg);
-	waitKey(0);   
 
     return 0;
 }
